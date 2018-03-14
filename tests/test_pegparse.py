563ae29dc5249b22bae0402d6465f893c2a9245d
Justin Li <justinnhli@gmail.com>
2018-03-14 00:00:18 -0700

    add test for ASTNode.descendants()

--- tests/test_pegparse.py
+++ tests/test_pegparse.py
@@ -1,6 +1,60 @@
 #!/usr/bin/env python3
 
+from textwrap import dedent
+
 from pegparse import EBNF_GRAMMAR, EBNF_DEFS, ASTNode, EBNFWalker
+from pegparse import create_parser
+
+def assert_equal(actual, expected, message_stem):
+    message = '\n'.join([
+        message_stem,
+        'expected: ' + str(expected),
+        'actual: ' + str(actual),
+    ])
+    assert actual == expected, message
+
+def test_descendants():
+    # setup
+    parser = create_parser(dedent('''
+        Expression = Operand ( Operator Operand )*;
+        Operand = ParenExpression
+                | Number;
+        ParenExpression = "(" Expression ")";
+        Operator = "+"
+                 | "-"
+                 | "*"
+                 | "/";
+        Number = ( digit )+;
+    '''.lstrip('\n').rstrip(' ')))
+    ast = parser.parse('(1+(2*3))-5', 'Expression')
+    message_stem = 'descendants() docstring is incorrect'
+    # tests
+    path = 'Operand/ParenExpression/Expression/Operand'
+    matches = [node.match for node in ast.descendants(path)]
+    assert_equal(['1', '(2*3)'], matches, message_stem)
+    descendants = ast.descendants('Operand')
+    assert_equal(
+        ['(1+(2*3))', '5'],
+        [node.match for node in descendants],
+        message_stem
+    )
+    assert_equal(
+        ['Operand', 'Operand'],
+        [node.term for node in descendants],
+        message_stem
+    )
+    descendants = ast.descendants('Operand/*')
+    assert_equal(
+        ['(1+(2*3))', '5'],
+        [node.match for node in descendants],
+        message_stem
+    )
+    assert_equal(
+        ['ParenExpression', 'Number'],
+        [node.term for node in descendants],
+        message_stem
+    )
+
 
 
 def test_ebnf_walker():
@@ -18,6 +72,7 @@ def test_ebnf_representation():
 
 
 def main():
+    test_descendants()
     test_ebnf_walker()
     test_ebnf_representation()
 
