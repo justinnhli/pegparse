fad77c5c8acfff10d1ea8c4d59f2ec082f968f99
Justin Li <justinnhli@gmail.com>
2021-05-27 15:03:41 -0700

    expose ASTNode for type hinting

--- pegparse/__init__.py
+++ pegparse/__init__.py
@@ -1,3 +1,3 @@
 """A Pack Rat Parsing Expression Grammer parser."""
 
-from .pegparse import create_parser, create_parser_from_file, ASTWalker
+from .pegparse import create_parser, create_parser_from_file, ASTWalker, ASTNode
